de30ecff1c7dd870d97dee43f931b8467c110192
kamiken <kentaro.kamiya.0425@gmail.com>
2023-08-08 18:46:34 +0900

    backup

--- docker/hadoop/start.sh
+++ docker/hadoop/start.sh
@@ -10,11 +10,11 @@ docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
 docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} pull
 sleep 5
 
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d --build namenode datanode resourcemanager nodemanager historyserver
+docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d --build namenode datanode1 datanode2 datanode3 resourcemanager nodemanager historyserver
 sleep 5
 docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d hive-server hive-metastore hive-metastore-postgresql
 sleep 5
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d spark-master spark-worker
+docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d --build spark-master spark-worker-1 spark-worker-2
 sleep 15
 
 docker exec -it namenode bash -c 'hdfs dfsadmin -safemode leave && hdfs dfs -mkdir -p /user/spark/applicationHistory'
