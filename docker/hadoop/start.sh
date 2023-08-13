883e2e82f0a23102a2cb060cd76395a79c3c3324
kamiken <kentaro.kamiya.0425@gmail.com>
2023-08-13 13:57:37 +0900

    add spark workers

--- docker/hadoop/start.sh
+++ docker/hadoop/start.sh
@@ -10,11 +10,11 @@ docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
 docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} pull
 sleep 5
 
-docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d --build namenode datanode1 datanode2 datanode3 resourcemanager nodemanager historyserver
+docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d namenode datanode1 datanode2 datanode3 resourcemanager nodemanager historyserver
 sleep 5
-docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d --build hive-server hive-metastore hive-metastore-postgresql
+docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d hive-server hive-metastore hive-metastore-postgresql
 sleep 5
-docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d --build spark-master spark-worker-1 spark-worker-2
+docker compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d spark-master spark-worker-1 spark-worker-2 spark-worker-3 spark-worker-4 spark-worker-5
 sleep 15
 
 docker exec -it namenode bash -c 'hdfs dfsadmin -safemode leave && hdfs dfs -mkdir -p /user/spark/applicationHistory'
