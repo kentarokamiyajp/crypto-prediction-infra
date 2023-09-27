07d93296b87c87a6ccfdc678beb0f88770a0b48e
kamiken <kentaro.kamiya.0425@gmail.com>
2023-09-27 21:37:04 +0900

    add python container

---
+++ docker/python_ubuntu/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ -d "/home/pyuser/.pyenv" ]; then
+    echo "pyenv already exists"
+else
+    cd /home/pyuser || exit
+
+    # install pyenv
+    git clone https://github.com/pyenv/pyenv.git /home/pyuser/.pyenv
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    if command -v pyenv 1>/dev/null 2>&1; then
+        eval "$(pyenv init -)"
+    fi
+
+    # install virtualenv
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+    eval "$(pyenv virtualenv-init -)"
+
+    # create python env
+    pyenv install 3.10.9
+    pyenv virtualenv 3.10.9 crypto
+    pyenv local crypto
+    pip install -r /home/pyuser/src/requirements.txt
+    python3.10 -m pip install --upgrade pip
+
+    cat /home/pyuser/init_bashrc >>/home/pyuser/.bashrc
+
+fi
